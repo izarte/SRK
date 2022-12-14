f694380bfee8080742f2ee590e7bd30525cf2d63
inigo183 <inigozarate01@gmail.com>
2022-12-14 02:40:58 +0100

    Add collision logic and commented subscription to speed

--- UnityProyect/Assets/Controller.cs
+++ UnityProyect/Assets/Controller.cs
@@ -8,12 +8,14 @@ using System.IO;
 using TMPro;
 using RosMessageTypes.SrkMove;
 using TwistMsg = RosMessageTypes.Geometry.TwistMsg;
+using BoolMsg = RosMessageTypes.Std.BoolMsg;
 
 public class Controller : MonoBehaviour
 {
     public ROSConnection ros;
     public float publishMessageFrequency = 0.5f;
-    public string topicName = "/cmd_vel";
+    public string topicNamePub = "/cmd_vel";
+    public string topicNameSub = "/collision";
     public const float MAX_LINEAR = 2;
     public const float MAX_AGULAR = 1;
     public const float MIN_LINEAR = 0.1f;
@@ -26,10 +28,12 @@ public class Controller : MonoBehaviour
     private float linear_factor = 1;
     private float angular_speed = 0;
     private float angular_factor = 1;
+    private bool collision = false;
 
     void Start()
     {
-        ros.RegisterPublisher<TwistMsg>(topicName);
+        ros.RegisterPublisher<TwistMsg>(topicNamePub);
+        ros.Subscribe<BoolMsg>(topicNameSub, Collision); 
         resetMsg();
     }
 
@@ -51,15 +55,15 @@ public class Controller : MonoBehaviour
             linear_factor = increase_factor(linear_speed, -0.2f, MAX_LINEAR, MIN_LINEAR);
             Debug.Log("changed speed factor" + linear_factor);
         }
-        linear_speed = calculate_speed(joystick.x, linear_factor);
-        angular_speed = calculate_speed(joystick.y, angular_factor);
+        linear_speed = collision ? 0 : calculate_speed(joystick.y, linear_factor);
+        angular_speed = calculate_speed(joystick.x, angular_factor);
 
         if (timeElapsed > publishMessageFrequency)
         {
             msg.linear.x = linear_speed;
             msg.angular.z = angular_speed;
 
-            ros.Publish(topicName, msg);
+            ros.Publish(topicNamePub, msg);
 
             timeElapsed = 0;
         }
@@ -73,6 +77,11 @@ public class Controller : MonoBehaviour
         msg.angular.x = 0.0f;
         msg.angular.y = 0.0f;
         msg.angular.z = 0.0f;
+    }
+
+    private void Collision(BoolMsg data)
+    {
+        collision = data.data;
     }
 
     private float calculate_speed(float input, float factor)
--- UnityProyect/Assets/Velocidad.cs
+++ UnityProyect/Assets/Velocidad.cs
@@ -15,13 +15,21 @@ public class Velocidad : MonoBehaviour{
     public ROSConnection ros;
     public Text textoVelocidad;
     public Controller controller;
+    // public string topicNameSub = "/command_base/commands/veloctity";
 
     private string linear_speed = "0";
     private string rotation_speed = "0";
     
     void Start(){
         textoVelocidad = GetComponent<Text>();
+        // ros.Subscribe<TwistMsg>(topicNameSub, Callback);
     }
+
+    // private void Callback(TwistMsg data)
+    // {
+    //     linear_speed = data.linear;
+    //     rotation_speed = data.angular;
+    // }
 
     void Update()
     {
